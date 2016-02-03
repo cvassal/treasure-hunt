b0dc876b14863f263da0584f04e7825d5649f39c
cedric <cedric.vassal@valtech.fr>
2016-02-03 19:40:49 +0100

    Refacto file reading with java 8 Files

--- src/main/java/data/board/BoardFileReader.java
+++ src/main/java/data/board/BoardFileReader.java
@@ -2,24 +2,26 @@ package data.board;
 
 import data.FileObjectMapper;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Objects;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public abstract class BoardFileReader<T> implements FileObjectMapper<T> {
 
     public T read(File file) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            return reader.lines().
-                    map(this::mapToObject).
-                    filter(Objects::nonNull).
-                    collect(Collectors.toList()).get(0);
-        } catch (Exception e) {
+        try {
+            return Files.readAllLines(Paths.get(file.toURI()))
+                    .stream()
+                    .map(this::mapToObject)
+                    .filter(Objects::nonNull)
+                    .collect(toList()).get(0);
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
         return null;
     }
 }
